a655853fc181ebfe6c3a6b62aeb9183db2bd18ba
AleXCBRX12 <bolanosdelacruz.miltonalejandro@utacapulco.edu.mx>
2026-03-15 12:47:06 -0600

    Corrigiendo configuración de base de datos SQL para Render

--- app_django/Django_inte/config/settings.py
+++ app_django/Django_inte/config/settings.py
@@ -84,12 +84,22 @@ TEMPLATES = [
 # ========================
 import dj_database_url
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
-        conn_max_age=600
-    )
-}
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # ========================
 # PASSWORD VALIDATORS
