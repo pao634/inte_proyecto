e117e51f416361dea4ee0acd221a425ff0e335ca
AleXCBRX12 <bolanosdelacruz.miltonalejandro@utacapulco.edu.mx>
2026-03-15 12:23:57 -0600

    Corrigiendo nombre de base de datos a milton_usuario y añadiendo dependencias

--- app_django/Django_inte/config/database/mongo.py
+++ app_django/Django_inte/config/database/mongo.py
@@ -14,7 +14,7 @@ class MongoDB:
         try:
             self.client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000)
             self.client.admin.command("ping")
-            self.db = self.client["202304042_db_user"]
+            self.db = self.client["milton_usuario"]
             self.fs = gridfs.GridFS(self.db)
             print("Conexión a MongoDB exitosa")
         except ConnectionFailure as e:
